f32a6c55fd8cc205bab3576e61cf1504af9a0bef
Soumajyoti Sarkar <103195837+soumajyoti02@users.noreply.github.com>
2023-03-01 19:39:30 +0530

    Create 019_Table_Navigation_35.js

---
+++ 019_Table_Navigation_35.js
@@ -0,0 +1,55 @@
+// HTML
+
+<body>
+  <div class="container my-4">
+    <table class="table">
+      <caption>Table of legends</caption>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</body>
+
+
+// JAVASCRIPT
+
+
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead.firstElementChild)
+console.log(t.tFoot)
+console.log(t.tBodies)
+console.log(t.rows[1].rowIndex)
+
+
+
+
+
+
