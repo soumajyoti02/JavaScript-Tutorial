caaa1871d4d42983fa774d13bd2acb9f91f69448
Soumajyoti Sarkar <103195837+soumajyoti02@users.noreply.github.com>
2023-03-01 19:32:31 +0530

    Create 017_Siblings_Parent_33.js

---
+++ 017_Siblings_Parent_33.js
@@ -0,0 +1,16 @@
+// HTML
+<body>
+  <div>
+    <div class="first">first</div>
+    <div class="second">second</div>
+  </div>
+  <script src="script.js"></script>
+</body>
+
+// JAVASCRIPT
+
+console.log(document.body.firstChild)
+a = document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
